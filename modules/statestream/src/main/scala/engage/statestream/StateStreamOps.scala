3c2e100d09b19fef48b51700bcbb2820edf82a80
Javier Lührs <jluhrs@gemini.edu>
2022-06-16 15:28:39 -0400

    Created generic execution engine.

--- modules/statestream/src/main/scala/engage/statestream/StateStreamOps.scala
+++ modules/statestream/src/main/scala/engage/statestream/StateStreamOps.scala
@@ -1,21 +1,28 @@
+// Copyright (c) 2016-2021 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
 package engage.statestream
 
-import cats.Functor
-import fs2.{Pipe, Stream}
+import cats.data.State
+import cats.{ Applicative, Functor }
 
 object StateStreamOps {
-  implicit def stateStreamFunctor[F[_]: Functor, S, I, O]: Functor[StateStream[F, S, I, *]] = new Functor[StateStream[F, S, I, *]] {
-    override def map[A, B](fa: StateStream[F, S, I, A])(f: A => B): StateStream[F, S, I, B] = new StateStream[F, S, I, B] {
-      override def getState: StateStream[F, S, I, S] = fa.getState
 
-      override def setState(s: S): StateStream[F, S, I, Unit] = fa.setState(s)
-
-      override def modifyState(f: S => S): StateStream[F, S, I, Unit] = fa.modifyState(f)
-
-      override def compile(s0: S): Pipe[F, I, B] = { a: Stream[F, I] =>
-        fa.compile(s0)(a).map(f)
-      }
+  implicit def stateStreamFunctor[F[_], S, I, O]: Functor[StateStream[F, S, I, *]] =
+    new Functor[StateStream[F, S, I, *]] {
+      override def map[A, B](fa: StateStream[F, S, I, A])(f: A => B): StateStream[F, S, I, B] =
+        new StateStream[F, S, I, B](i => fa.h(i).map(f))
     }
-  }
+
+  implicit def stateStreamApplicative[F[_], S, I, O]: Applicative[StateStream[F, S, I, *]] =
+    new Applicative[StateStream[F, S, I, *]] {
+      override def pure[A](x: A): StateStream[F, S, I, A] = new StateStream[F, S, I, A]((_: I) =>
+        State.pure[S, A](x)
+      )
+
+      override def ap[A, B](ff: StateStream[F, S, I, A => B])(
+        fa:                     StateStream[F, S, I, A]
+      ): StateStream[F, S, I, B] = ???
+    }
 
 }
--- modules/statestream/src/test/scala/engage/statestream/StateStreamSpec.scala
+++ modules/statestream/src/test/scala/engage/statestream/StateStreamSpec.scala
@@ -1,41 +1,40 @@
+// Copyright (c) 2016-2021 Association of Universities for Research in Astronomy, Inc. (AURA)
+// For license information see LICENSE or https://opensource.org/licenses/BSD-3-Clause
+
 package engage.statestream
 
 import cats.data.State
 import cats.effect.IO
+import cats.syntax.eq._
 import munit.CatsEffectSuite
 import fs2.Stream
 
 class StateStreamSpec extends CatsEffectSuite {
-import StateStreamSpec._
+  import StateStreamSpec._
   test("statestream") {
     assertIOBoolean {
       val i = Stream[IO, Int](1, 2, 3)
 
-      val p = StateStream.compile[IO, TestState, Int, Msg]{ x =>
-        State { s =>
+      val p = new StateStream[IO, TestState, Int, Msg]((x: Int) =>
+        State[TestState, Msg] { s =>
           val c = s.getOrElse(x, 0)
           (
-            s.updated(x, c+1),
-            Stream.emits[IO, Msg](List.range(0, x).map(Msg(x, _)))
+            s.updated(x, c + 1),
+            Msg(x, x.toString)
           )
         }
-      }(Map.empty[Int, Int])
+      ).compile(Map.empty[Int, Int])
 
-      p(i).compile.toList.map(check)
+      p(i).compile.toList.map(l => check(l.map(_._2)))
     }
   }
 
-  private def check(r: List[Msg]): Boolean = {
-    val g = r.groupBy(_.from)
-    g.get(1).exists(_.length == 1) &&
-      g.get(2).exists(_.length == 2) &&
-      g.get(3).exists(_.length == 3)
-  }
+  private def check(r: List[Msg]): Boolean =
+    r.forall(m => m.from.toString === m.txt)
 
 }
 
 object StateStreamSpec {
-  final case class Msg(from: Int, seq: Int)
+  final case class Msg(from: Int, txt: String)
   type TestState = Map[Int, Int]
 }
-
