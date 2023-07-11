8fa388250e014a1b1e75b6f4a30bd6e0b2e4c623
Diego Gomez <diego.gomez@noirlab.edu>
2023-07-11 13:02:28 -0400

    Unit test added

--- modules/server/src/test/scala/navigate/server/tcs/TcsBaseControllerEpicsSpec.scala
+++ modules/server/src/test/scala/navigate/server/tcs/TcsBaseControllerEpicsSpec.scala
@@ -149,7 +149,7 @@ class TcsBaseControllerEpicsSpec extends CatsEffectSuite {
       iaa = Angle.fromDoubleDegrees(123.45),
       focusOffset = Distance.fromLongMicrometers(2344),
       agName = "gmos",
-      origin = (Distance.fromLongMicrometers(4567), Distance.fromLongMicrometers(-8901))
+      origin = Origin(Distance.fromLongMicrometers(4567), Distance.fromLongMicrometers(-8901))
     )
 
     for {
@@ -256,6 +256,63 @@ class TcsBaseControllerEpicsSpec extends CatsEffectSuite {
       assert(
         rs.rotator.iaa.value.exists(x =>
           compareDouble(x.toDouble, instrumentSpecifics.iaa.toDoubleDegrees)
+        )
+      )
+    }
+  }
+
+  test("InstrumentSpecifics command") {
+    val instrumentSpecifics: InstrumentSpecifics = InstrumentSpecifics(
+      iaa = Angle.fromDoubleDegrees(123.45),
+      focusOffset = Distance.fromLongMicrometers(2344),
+      agName = "gmos",
+      origin = Origin(Distance.fromLongMicrometers(4567), Distance.fromLongMicrometers(-8901))
+    )
+
+    for {
+      x        <- createController
+      (st, ctr) = x
+      _        <- ctr.instrumentSpecifics(instrumentSpecifics)
+      rs       <- st.get
+    } yield {
+      assert(
+        rs.rotator.iaa.value.exists(x =>
+          compareDouble(x.toDouble, instrumentSpecifics.iaa.toDoubleDegrees)
+        )
+      )
+      assert(
+        rs.focusOffset.value.exists(x =>
+          compareDouble(x.toDouble, instrumentSpecifics.focusOffset.toMicrometers.value.toDouble)
+        )
+      )
+      assert(
+        rs.origin.xa.value.exists(x =>
+          compareDouble(x.toDouble, instrumentSpecifics.origin.x.toMicrometers.value.toDouble)
+        )
+      )
+      assert(
+        rs.origin.xb.value.exists(x =>
+          compareDouble(x.toDouble, instrumentSpecifics.origin.x.toMicrometers.value.toDouble)
+        )
+      )
+      assert(
+        rs.origin.xc.value.exists(x =>
+          compareDouble(x.toDouble, instrumentSpecifics.origin.x.toMicrometers.value.toDouble)
+        )
+      )
+      assert(
+        rs.origin.ya.value.exists(x =>
+          compareDouble(x.toDouble, instrumentSpecifics.origin.y.toMicrometers.value.toDouble)
+        )
+      )
+      assert(
+        rs.origin.yb.value.exists(x =>
+          compareDouble(x.toDouble, instrumentSpecifics.origin.y.toMicrometers.value.toDouble)
+        )
+      )
+      assert(
+        rs.origin.yc.value.exists(x =>
+          compareDouble(x.toDouble, instrumentSpecifics.origin.y.toMicrometers.value.toDouble)
         )
       )
     }
