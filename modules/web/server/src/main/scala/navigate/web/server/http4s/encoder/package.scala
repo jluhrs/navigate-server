5101a5ad88d0d803e4dbce1d9315c94d203473c7
Hugo van Rijswijk <git@hugovr.nl>
2024-07-31 11:04:40 -0400

    Truncate logmessage timestamp value

--- modules/web/server/src/main/scala/navigate/web/server/http4s/encoder/package.scala
+++ modules/web/server/src/main/scala/navigate/web/server/http4s/encoder/package.scala
@@ -17,7 +17,7 @@ package object encoder {
 
   given Encoder[ILoggingEvent] = e =>
     Json.obj(
-      "timestamp" -> Timestamp.fromInstant(e.getInstant).getOrElse(Timestamp.Min).asJson,
+      "timestamp" -> Timestamp.fromInstantTruncated(e.getInstant).getOrElse(Timestamp.Min).asJson,
       "level"     -> e.getLevel.toString.asJson,
       "thread"    -> e.getThreadName.asJson,
       "message"   -> e.getFormattedMessage.asJson
