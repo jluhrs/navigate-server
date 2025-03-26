a2670bfcb31d12f56189252b70ab7d7b67bad483
Carlos Quiroz <carlos.quiroz@noirlab.edu>
2025-03-26 17:38:54 -0300

    Add test for acq adjustment subscription

--- modules/web/server/src/test/scala/navigate/web/server/http4s/NavigateMappingsSuite.scala
+++ modules/web/server/src/test/scala/navigate/web/server/http4s/NavigateMappingsSuite.scala
@@ -35,6 +35,7 @@ import munit.CatsEffectSuite
 import navigate.model.AcquisitionAdjustment
 import navigate.model.NavigateEvent
 import navigate.model.NavigateState
+import navigate.model.enums.AcquisitionAdjustmentOption
 import navigate.model.enums.DomeMode
 import navigate.model.enums.LightSource
 import navigate.model.enums.ShutterMode
@@ -993,6 +994,87 @@ class NavigateMappingsSuite extends CatsEffectSuite {
 
   }
 
+  test("Provide acquisition adjustment state subscription") {
+    import lucuma.core.math.Angle
+    import lucuma.core.math.Offset
+    import navigate.model.enums.AcquisitionAdjustmentOption
+
+    val changes: List[AcquisitionAdjustment] = List(
+      AcquisitionAdjustment(
+        offset = Offset.signedDecimalArcseconds.reverseGet(2, 3),
+        ipa = Angle.fromDoubleArcseconds(0.1).some,
+        iaa = Angle.fromDoubleArcseconds(0.2).some,
+        command = AcquisitionAdjustmentOption.AskUser
+      ),
+      AcquisitionAdjustment(
+        offset = Offset.signedDecimalArcseconds.reverseGet(4, 5),
+        ipa = Angle.fromDoubleArcseconds(0.2).some,
+        iaa = Angle.fromDoubleArcseconds(0.3).some,
+        command = AcquisitionAdjustmentOption.UserConfirms
+      ),
+      AcquisitionAdjustment(
+        offset = Offset.signedDecimalArcseconds.reverseGet(5, 6),
+        ipa = None,
+        iaa = None,
+        command = AcquisitionAdjustmentOption.AskUser
+      )
+    )
+
+    def putAcquisitionAdjustmentUpdates(topic: Topic[IO, AcquisitionAdjustment]): IO[Unit] =
+      changes.map(topic.publish1).sequence.void
+
+    val s: IO[List[Result[AcquisitionAdjustment]]] = for {
+      eng <- buildServer
+      log <- Topic[IO, ILoggingEvent]
+      gd  <- Topic[IO, GuideState]
+      gq  <- Topic[IO, GuidersQualityValues]
+      ts  <- Topic[IO, TelescopeState]
+      aa  <- Topic[IO, AcquisitionAdjustment]
+      lb  <- Ref.empty[IO, Seq[ILoggingEvent]]
+      mp  <- NavigateMappings[IO](eng, log, gd, gq, ts, aa, lb)
+      up  <- mp.compileAndRunSubscription(
+               """
+             | subscription {
+             |   acquisitionAdjustmentState {
+             |     offset {
+             |       p {
+             |         milliarcseconds
+             |       }
+             |       q {
+             |         milliarcseconds
+             |       }
+             |     }
+             |     ipa {
+             |       microarcseconds
+             |     }
+             |     iaa {
+             |       milliarcseconds
+             |     }
+             |     command
+             |   }
+             | }
+             |""".stripMargin
+             ).map { a =>
+               println(a)
+               a.hcursor
+                 .downField("data")
+                 .downField("acquisitionAdjustmentState")
+                 .as[AcquisitionAdjustment]
+             }.take(changes.length)
+               .compile
+               .toList
+               .timeout(Duration.fromNanos(10e9))
+               .both(putAcquisitionAdjustmentUpdates(aa).delayBy(Duration.fromNanos(1e9)))
+               .map(_._1)
+    } yield up
+
+    s.map { l =>
+      val g: List[AcquisitionAdjustment] = l.collect { case Right(a) => a }
+      assertEquals(g.length, changes.length)
+      assertEquals(g, changes)
+    }
+  }
+
   test("Process guide disable command") {
     for {
       eng <- buildServer
@@ -1423,6 +1505,8 @@ class NavigateMappingsSuite extends CatsEffectSuite {
 }
 
 object NavigateMappingsTest {
+  import lucuma.odb.json.offset.query.given
+  import lucuma.odb.json.angle.query.given
 
   val dummyClient = Client.fromHttpApp(HttpApp.notFound[IO])
 
@@ -1681,5 +1765,13 @@ object NavigateMappingsTest {
     for {
       swp <- h.downField("onSwappedTarget").as[Boolean]
     } yield NavigateState(swp)
+
+  given Decoder[AcquisitionAdjustment] = h =>
+    for {
+      offset <- h.downField("offset").as[Offset]
+      ipa    <- h.downField("ipa").as[Option[Angle]]
+      iaa    <- h.downField("iaa").as[Option[Angle]]
+      cmd    <- h.downField("command").as[AcquisitionAdjustmentOption]
+    } yield AcquisitionAdjustment(offset = offset, ipa = ipa, iaa = iaa, command = cmd)
 
 }
