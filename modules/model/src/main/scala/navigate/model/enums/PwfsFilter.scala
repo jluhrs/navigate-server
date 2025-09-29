55f13aa61ff0a94ee949148e14b28f1b3cee5e9c
Javier Lührs <jluhrsm@gmail.com>
2025-09-29 11:40:23 -0300

    SC-6891 Fixed PWFS Red filter wavelength

--- modules/model/src/main/scala/navigate/model/enums/PwfsFilter.scala
+++ modules/model/src/main/scala/navigate/model/enums/PwfsFilter.scala
@@ -10,7 +10,7 @@ enum PwfsFilter(val tag: String, val wavel: Wavelength) derives Enumerated {
   case Neutral extends PwfsFilter("Neutral", Wavelength.unsafeFromIntPicometers(650_000))
   case Blue    extends PwfsFilter("Blue", Wavelength.unsafeFromIntPicometers(450_000))
   case Green   extends PwfsFilter("Green", Wavelength.unsafeFromIntPicometers(550_000))
-  case Red     extends PwfsFilter("Red", Wavelength.unsafeFromIntPicometers(600_00))
+  case Red     extends PwfsFilter("Red", Wavelength.unsafeFromIntPicometers(600_000))
   case Red04   extends PwfsFilter("Red04", Wavelength.unsafeFromIntPicometers(640_000))
   case Red1    extends PwfsFilter("Red1", Wavelength.unsafeFromIntPicometers(700_000))
 }
